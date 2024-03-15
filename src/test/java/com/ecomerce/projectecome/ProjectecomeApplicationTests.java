d0dbd5ecfa6ba0c0ee8df7132b5bc9a214e176f8
cheikh-sidi-taher <cheikhmed2019a@gmail.com>
2024-03-15 23:43:51 +0000

    first commit

---
+++ src/test/java/com/ecomerce/projectecome/ProjectecomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.projectecome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectecomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
